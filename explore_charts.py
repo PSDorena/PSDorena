d019db929187df544c4b9c923356a2383dc91886
SaraPena <pena.e.sara@gmail.com>
2019-12-11 16:17:12 -0600

    Update explore_charts.py

--- explore_charts.py
+++ explore_charts.py
@@ -1,31 +1,30 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def donut_chart(x)
+def donut_chart(recipe, data):
 
     fig, ax = plt.subplots(figsize=(6, 3), subplot_kw=dict(aspect="equal"))
 
-    recipe = x
+    recipe = recipe
 
-    data = [float(x.split()[0]) for x in recipe]
-    ingredients = [x.split()[-1] for x in recipe]
+    data = data
 
+    wedges, texts = ax.pie(data, wedgeprops=dict(width=0.5), startangle=-40)
 
-    def func(pct, allvals):
-        absolute = int(pct/100.*np.sum(allvals))
-        return "{:.1f}%\n({:d} g)".format(pct, absolute)
+    bbox_props = dict(boxstyle="square,pad=0.3", fc="w", ec="k", lw=0.72)
+    kw = dict(arrowprops=dict(arrowstyle="-"),
+              bbox=bbox_props, zorder=0, va="center")
 
+    for i, p in enumerate(wedges):
+        ang = (p.theta2 - p.theta1)/2. + p.theta1
+        y = np.sin(np.deg2rad(ang))
+        x = np.cos(np.deg2rad(ang))
+        horizontalalignment = {-1: "right", 1: "left"}[int(np.sign(x))]
+        connectionstyle = "angle,angleA=0,angleB={}".format(ang)
+        kw["arrowprops"].update({"connectionstyle": connectionstyle})
+        ax.annotate(recipe[i], xy=(x, y), xytext=(1.35*np.sign(x), 1.4*y),
+                    horizontalalignment=horizontalalignment, **kw)
 
-    wedges, texts, autotexts = ax.pie(data, autopct=lambda pct: func(pct, data),
-                                      textprops=dict(color="w"))
-
-    ax.legend(wedges, ingredients,
-              title="Languages",
-              loc="center left",
-              bbox_to_anchor=(1, 0, 0.5, 1))
-
-    plt.setp(autotexts, size=8, weight="bold")
-
-    ax.set_title("Types of Repository Languages")
+    ax.set_title("Matplotlib bakery: A donut")
 
     plt.show()
